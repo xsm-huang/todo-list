a5811dce0bbc3b417fe473fbfdf70ae9923df936
xsm-huang <1012973656@qq.com>
2022-03-28 16:10:53 +0800

    各组件间通信，使用app传值，基础版

---
+++ src/components/ListItem.vue
@@ -0,0 +1,61 @@
+<template>
+    <li>
+        <label>
+            <!-- <input type="checkbox" v-model="todo.done" /> -->
+            <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)" />
+            <span>{{ todo.title }}</span>
+        </label>
+        <button class="btn btn-danger" style="display: none">删除</button>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'ListItem',
+    // 声明接收 todo 对象
+    props: ['todo', 'checkTodo'],
+    methods: {
+        handleCheck(id) {
+            // 通知 app 组件将对应的 todo 对象的 done 值取反
+            this.checkTodo(id);
+        },
+    },
+};
+</script>
+
+<style scoped>
+/*item*/
+li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+}
+
+li label {
+    float: left;
+    cursor: pointer;
+}
+
+li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+}
+
+li button {
+    float: right;
+    display: none;
+    margin-top: 3px;
+}
+
+li:before {
+    content: initial;
+}
+
+li:last-child {
+    border-bottom: none;
+}
+</style>
